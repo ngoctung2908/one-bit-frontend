8686385e37d74030eb80b25a764f19ad859d228a
quoc.hc <deptraikosoai2908@gmail.com>
2022-06-29 10:47:07 +0700

    custom button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,78 +5,39 @@ interface BtnPropsWithChildren {}
 interface BtnProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     BtnPropsWithChildren {
-  block?: boolean
   children: React.ReactNode
   className?: string
-  color?: 'primary' | 'success' | 'danger' | 'warning' | 'indigo' | 'dark'
   disabled?: boolean
   outline?: boolean
-  rounded?: boolean
   size?: 'sm' | 'md' | 'lg'
-  submit?: boolean
 }
 
 type ButtonRef = React.ForwardedRef<HTMLButtonElement>
 
 const style = {
-  rounded: `rounded-full`,
-  block: `flex justify-center w-full`,
-  default: `text-white focus:outline-none shadow font-medium transition ease-in duration-200`,
+  default: `text-white rounded-[3px] font-bold leading-6 tracking-[1px] shadow flex items-center justify-center gap-x-2 transition ease-in duration-200`,
   disabled: `opacity-60 cursor-not-allowed`,
   sizes: {
-    sm: 'px-6 py-1 text-sm',
-    md: 'px-6 py-2',
-    lg: 'px-6 py-3 text-lg',
+    sm: 'px-[14px] py-2',
+    md: 'px-[18px] py-2.5',
+    lg: 'px-7 py-4',
   },
   color: {
     primary: {
-      bg: `bg-blue-700 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:ring-offset-blue-200`,
-      outline: `border-blue-700 border-2 text-blue-700 active:bg-blue-700 active:text-white`,
-    },
-    success: {
-      bg: `bg-green-700 focus:ring-2 focus:ring-offset-2 focus:ring-green-700 focus:ring-offset-green-200`,
-      outline: `border-green-700 border-2 text-green-700 active:bg-green-700 active:text-white`,
-    },
-    danger: {
-      bg: `bg-red-600 focus:ring-2 focus:ring-offset-2 focus:ring-red-600 focus:ring-offset-red-200`,
-      outline: `border-red-600 border-2 text-red-600 active:bg-red-600 active:text-white`,
-    },
-    dark: {
-      bg: `bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 focus:ring-offset-gray-200`,
-      outline: `border-black border-2 text-gray-900 active:bg-black active:text-white`,
-    },
-    warning: {
-      bg: `bg-yellow-500 focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 focus:ring-offset-yellow-200`,
-      outline: `border-yellow-500 border-2 text-yellow-500 active:bg-yellow-500 active:text-white`,
-    },
-    indigo: {
-      bg: `bg-indigo-900 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-900 focus:ring-offset-indigo-200`,
-      outline: `border-indigo-900 border-2 text-indigo-900 active:bg-indigo-900 active:text-white`,
+      bg: `bg-accent-dark-2 hover:bg-accent-dark-0 focus:focus-shadow`,
+      outline: `bg-transparent border border-solid border-neutral-8 focus:focus-shadow`,
     },
   },
 }
 
-const colors = (outline: boolean) => ({
-  primary: outline ? style.color.primary.outline : style.color.primary.bg,
-  success: outline ? style.color.success.outline : style.color.success.bg,
-  danger: outline ? style.color.danger.outline : style.color.danger.bg,
-  dark: outline ? style.color.dark.outline : style.color.dark.bg,
-  warning: outline ? style.color.warning.outline : style.color.warning.bg,
-  indigo: outline ? style.color.indigo.outline : style.color.indigo.bg,
-})
-
 const Button = forwardRef(
   (
     {
-      block = false,
       children,
       className,
-      color,
       disabled = false,
-      outline,
-      rounded,
+      outline = false,
       size = 'md',
-      submit,
       ...props
     }: BtnProps,
     ref: ButtonRef
@@ -84,12 +45,12 @@ const Button = forwardRef(
     <button
       ref={ref}
       {...props}
-      type={submit ? 'submit' : 'button'}
+      type="button"
       disabled={disabled}
-      className={`${className} ${block ? style.block : ''}
+      className={`${className}
         ${disabled ? style.disabled : ''} ${style.sizes[size]} 
-        ${style.default} ${rounded ? style.rounded : 'rounded'}
-        ${color ? colors(outline)[color] : colors(outline).dark}`}
+        ${style.default}
+        ${outline ? style.color.primary.outline : style.color.primary.bg}`}
     >
       {children}
     </button>
