8050230f0876b2f24b19ed7ad91f4a8fb1cb66ab
LeeThang <lethang31897@gmail.com>
2022-07-01 17:34:05 +0700

    commit footer and partner

--- src/components/layouts/landing/layout.tsx
+++ src/components/layouts/landing/layout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import { Sider } from './sider/sider'
 import { Nav } from './nav/nav'
+import Footer from './footer/footer'
+import { Partner } from 'components/partner/partner'
 
 export const Layout = () => {
   return (
-    <div className="bg-accent-dark-0 min-h-screen flex relative">
+    <div className=" bg-accent-dark-0 min-h-screen flex relative">
       <Sider />
       <div className="flex-1 p-8">
         <Nav />
         <Outlet />
+        <Partner />
+        <Footer />
       </div>
     </div>
   )
---
+++ src/components/partner/partner.tsx
@@ -0,0 +1,20 @@
+import React, { memo } from 'react'
+import logoFacebook from 'assets/images/logo-facebook.svg'
+import logoForbes from 'assets/images/logo-forbes.svg'
+import logoGithub from 'assets/images/logo-github.svg'
+import logoGoogle from 'assets/images/logo-google.svg'
+import logoMicrosoft from 'assets/images/logo-microsoft.svg'
+
+export const Partner = memo(() => {
+  return (
+    <div className="mt-48 flex flex-col justify-center items-center">
+      <div className="flex py-11 mt-11 mb-12 gap-x-14  items-center justify-center border-y border-accent-dark-2 border-solid   ">
+        <img src={logoGithub} alt="" />
+        <img src={logoForbes} alt="" />
+        <img src={logoGoogle} alt="" />
+        <img src={logoMicrosoft} alt="" />
+        <img src={logoFacebook} alt="" />
+      </div>
+    </div>
+  )
+})
