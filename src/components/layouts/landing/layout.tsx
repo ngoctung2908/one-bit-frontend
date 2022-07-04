628d916f55ebde72b55ff015958454e737f1efa7
tung.vn <vnt2908@gmail.com>
2022-07-04 10:06:36 +0700

    Update background

--- src/components/layouts/landing/layout.tsx
+++ src/components/layouts/landing/layout.tsx
@@ -6,7 +6,7 @@ import { Partner } from 'components/partner/partner'
 
 export const Layout = () => {
   return (
-    <div className=" bg-accent-dark-0 min-h-screen flex relative">
+    <div className="min-h-screen flex relative">
       <div className="min-h-screen flex relative overflow-x-hidden">
         <Sider />
         <div className="flex-1 p-8">
